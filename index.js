5e972219e4575ce3b3e5a3987d909e535e0e4014
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2023-10-30 22:19:58 +0530

    partials added

--- index.js
+++ index.js
@@ -16,14 +16,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  const randomAdj = adj[Math.floor(Math.random()*adj.length)]
-  const randomNoun =  noun[Math.floor(Math.random()*noun.length)]
-  res.render("index.ejs",{adjective : randomAdj,noun : randomNoun});
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  res.render("index.ejs", {
+    adjective: randomAdj,
+    noun: randomNoun,
+  });
 });
 
 app.listen(port, () => {
